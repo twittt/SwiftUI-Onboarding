9e2762329b7f680f2e5145a41d8d06ad9c9b8309
Tela Wittig <telawittig@gmail.com>
2024-08-11 15:43:00 -0500

    Initial Commit

---
+++ ImperativeSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ImperativeSwiftUI
+//
+//  Created by Tela Wittig on 8/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ImperativeSwiftUI/ImperativeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImperativeSwiftUIApp.swift
+//  ImperativeSwiftUI
+//
+//  Created by Tela Wittig on 8/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ImperativeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
